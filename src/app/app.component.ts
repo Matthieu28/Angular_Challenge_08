85a53cdf24c15a319d9ee2885fcbbb7345568059
Matthieu <Matthieu.Maginot@gmail.com>
2023-05-17 11:32:03 +0200

    onomatopia challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularAppChallenge_08';
+  title = 'Welcome !';
+
+  onomatopoeiaList: string[] = [];
+
+  onReceiveNewOnomatopia(newOnomatopia: string): void {
+    this.onomatopoeiaList.push(newOnomatopia)
+  }
 }
